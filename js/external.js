a949676a4093691b1ca2fb4d84887c42d692d926
Dustin Martinez <martinezdustin736@gmail.com>
2021-03-19 08:45:58 -0500

    JavaScript with HTML exercises

---
+++ js/external.js
@@ -0,0 +1,41 @@
+"use strict";
+// console.log("Hello from external JavaScript");
+//
+// alert("Welcome to my website");
+
+// var favoriteColor = prompt("What is your favorite color?")
+// alert(favoriteColor + " is my favorite color too")
+
+
+
+// var lmRentalDays = parseFloat(prompt("How many days did you rent Little Mermaid?"));
+// console.log(lmRentalDays);
+//
+// var bbRentalDays = parseFloat(prompt("How many days did you Brother Bear?"));
+// console.log(bbRentalDays);
+//
+// var hercRentalDays = parseFloat(prompt("How many days did you rent Hercules?"));
+// console.log(hercRentalDays);
+//
+// var rentalRate= 3;
+// var totalDue= alert("You owe " + (lmRentalDays + bbRentalDays + hercRentalDays) * rentalRate);
+
+
+// var gRate = parseFloat(prompt("What is your Google rate?"));
+// console.log(gRate)
+// var amazeRate = parseFloat(prompt("What is your Amazon rate?"));
+// console.log(amazeRate)
+// var faceRate = parseFloat(prompt("What is your Facebook rate?"));
+// console.log(faceRate);
+//
+// var gHours = parseFloat(prompt("How many hours did you work at Google?"));
+// console.log(gHours);
+// var amazeHours = parseFloat(prompt("How many hours did you work at Amazon?"));
+// console.log(amazeHours);
+// var faceHours = parseFloat(prompt("How many hours did you work at Facebook?"));
+// console.log(faceHours);
+//
+// var payForWeek= (gRate * gHours) + (faceRate * faceHours) + (amazeRate * amazeHours);
+//
+// alert("Your pay for the week is " + payForWeek);
+// console.log(payForWeek);
