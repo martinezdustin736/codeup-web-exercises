24c984469d94ad5ea8758e065fc61345b52df478
Dustin Martinez <martinezdustin736@gmail.com>
2021-03-25 15:50:19 -0500

    added Do While loop

--- js/while.js
+++ js/while.js
@@ -1,8 +1,30 @@
 "use strict";
 
-var i = 2;
+// var i = 2;
+//
+// while (i <= 65536) {
+//     console.log(i)
+//     i = i * 2;
+// }
 
-while (i <= 65536) {
-    console.log(i)
-    i = i * 2;
+// Do While
+
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+var conesAPerson = Math.floor(Math.random() * 5) + 1;
+
+console.log("I need to sell " + allCones + " cones to be able to go home")
+
+do {
+    allCones = allCones - conesAPerson;
+    console.log("Sold " + conesAPerson + " only " + allCones + " left")
+} while (conesAPerson <= allCones && allCones > 0);
+if (allCones <= conesAPerson) {
+    console.log("I have " + allCones + " left sorry i dont have enough for " + conesAPerson)
+    if (allCones === 0) {
+        console.log("Yay! I sold them all");
+    }
 }
+
+
