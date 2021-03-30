2f7dd2526a0b5e20905d346f4efaabeb8e1d32c7
Dustin Martinez <martinezdustin736@gmail.com>
2021-03-29 20:17:43 -0500

    arrays exercises

---
+++ js/iterating.js
@@ -0,0 +1,81 @@
+(function(){
+    "use strict";
+
+    /**
+     * TODO:
+     * Create an array of 4 people's names and store it in a variable called
+     * 'names'.
+     */
+
+    var names = ["David", "Joseph", "Evan", "Irvin"];
+
+    /**
+     * TODO:
+     * Create a log statement that will log the number of elements in the names
+     * array.
+     */
+
+    console.log([names.length]);
+
+    /**
+     * TODO:
+     * Create log statements that will print each of the names individually by
+     * accessing each element's index.
+     */
+
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
+
+    /**
+     * TODO:
+     * Write some code that uses a for loop to log every item in the names
+     * array.
+     */
+
+    // for (var i = 0; i < names.length; i++){
+    //     console.log(names);
+    // }
+
+    /**
+     * TODO:
+     * Refactor your above code to use a `forEach` loop
+     */
+        names.forEach(function(element, index, array) {
+            console.log(names);
+    })
+    /**
+     * TODO:
+     * Create the following three functions, each will accept an array and
+     * return an an element from it
+     * - first: returns the first item in the array
+     * - second: returns the second item in the array
+     * - last: returns the last item in the array
+     *
+     * Example:
+     *  > first([1, 2, 3, 4, 5]) // returns 1
+     *  > second([1, 2, 3, 4, 5]) // returns 2
+     *  > last([1, 2, 3, 4, 5]) // return 5
+     */
+
+
+function first () {
+        var numbers = [1, 2, 3, 4, 5]
+        return numbers [0]
+    }
+    console.log(first());
+
+    function second () {
+        var numbers = [1, 2, 3, 4, 5]
+        return numbers [1]
+    }
+    console.log(second());
+
+    function last () {
+        var numbers = [1, 2, 3, 4, 5]
+        return numbers [4]
+    }
+    console.log(last());
+
+})();
