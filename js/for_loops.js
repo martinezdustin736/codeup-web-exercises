206263c31c399e0e5082b5c7fa8d527aba3de56a
Dustin Martinez <martinezdustin736@gmail.com>
2021-04-13 09:20:33 -0500

    Practicing loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -23,9 +23,9 @@
     // }
 
 
-// for (var i = 1; i <= 9; i++) {
-//     console.log(i.toString().repeat(i));
-// }
+for (var i = 1; i <= 9; i++) {
+    console.log(i.toString().repeat(i));
+}
 
 
 // for (var i = 1; i < 10; i++) {
