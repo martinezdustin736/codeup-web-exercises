8480aa847c859d422b85683f8287ce61769be4aa
Dustin Martinez <martinezdustin736@gmail.com>
2021-05-24 22:56:33 -0500

    fixed fizz buzz iterator exercise complete

--- js/warmups.js
+++ js/warmups.js
@@ -1,19 +1,50 @@
 
 
 function fizzy (num){
-    for(let i = 1; i <=30; i++ ){
-    if(num % 3 ===0 && num % 5 === 0) {
+
+    for(let i = 1; i <= num; i++ ){
+    if(i % 3 ===0 && i % 5 === 0) {
         console.log("FizzBuzz");
-    } else if(num % 3 === 0) {
+    } else if(i % 3 === 0) {
         console.log("Fizz")
-    } else if (num % 5 === 0){
+    } else if (i % 5 === 0){
         console.log("Buzz")
     } else{
-        console.log(num)
+        console.log(i)
         }
 
     }
 }
 
-console.log(fizzy());
 
+fizzy(30);
+
+
+// (() => {
+
+    /**
+     *
+     * Often in programming interviews, you may be asked to solve a problem called “FizzBuzz.” FizzBuzz is a test of your ability to
+     * use conditional logic with a loop that counts up by one with every iteration. The challenge asks that you log the word “Fizz” if
+     * a number is evenly divisible by 3, “Buzz” if a number is evenly divisible by 5, “FizzBuzz” if it is divisible by both 3 and 5,
+     * and finally the number if it is not divisible by 3 or 5.
+     *
+     * This morning, I’d like you to write FizzBuzz in JavaScript using a for loop that goes until the number 30.
+     *
+     */
+//     const fizzBuzz = numTil => {
+//         for (let x = 1; x <= numTil; x++) {
+//             if (x % 3 === 0 && x % 5 === 0) {
+//                 console.log("FizzBuzz");
+//             } else if (x % 3 === 0) {
+//                 console.log("Fizz");
+//             } else if (x % 5 === 0) {
+//                 console.log("Buzz");
+//             } else {
+//                 console.log(x);
+//             }
+//         }
+//     }
+//
+//     fizzBuzz(30);
+// })();
