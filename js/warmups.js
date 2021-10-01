75a24c02934d2777b6962b0db379cb1e78ab67cb
Dustin Martinez <martinezdustin736@gmail.com>
2021-10-01 08:40:08 -0500

    functions practice

--- js/warmups.js
+++ js/warmups.js
@@ -228,6 +228,19 @@
 //
 // console.log(removeVow("Hi my name is Dustin"));
 
+const fibonacci = num => {
+    const result = [0, 1]
+
+    for (let i = 2; i <= num; i++){
+        const prevNum1 = result [i-1]
+        const prevNum2 = result [i-2]
+        result.push(prevNum1 + prevNum2)
+    }
+    console.log(result[num])
+}
+
+fibonacci(5);
+
 
 
 
